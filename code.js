644eaf7891ec41f00629bffe9dce664762112405
Casanova Geary <CasanovaGeary@Gmail.com>
2022-08-10 10:49:51 -0500

    Exercise 4 & 5, but I'm doing this wrong

--- code.js
+++ code.js
@@ -6,9 +6,16 @@
 */
 //Exercise #2, Step 2 I'm not confident doing. How do I make it red, other than making an obvious error?
 function sayHello() {
-    return "";
+    if(input === "Alex") {
+        return "Hello, Alex!";
+    } else if(input === "Pat") {
+        return "Hello, Pat!";
+    }else {
+        return "Hello, Jane!";
+    }
 }
-
+//LOST. I just noticed in Jasmine, if I click on "ran 1 0f 9" the result is red! What am I doing wrong?
+//Need instruction for this assignment.
 
 //FUNCTION EXPRESSION SYNTAX (ASSIGNING AN ANON FUNCTION TO A VARIABLE)//
 const helloWorld = function () {
--- tests.js
+++ tests.js
@@ -13,14 +13,33 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
-//sayHello()
+//Exercise #1 sayHello function defined
 describe('sayHello', function() {
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
 });
+//Exercise #2 Ensure function returns correct data type
 describe('sayHello', function() {
     it('should return a string when called.', function() {
         expect(typeof sayHello()).toBe("string");
+    });
+});
+//Exercise #3 Add test confirming actual vs. expected output
+describe('sayHello', function() {
+    it('should return a string "Hello, Jane!"', function() { //or 'should return a string'?
+        expect(typeof sayHello()).toBe("Hello, Jane!");
+    });
+});
+//Exercise #4 Add another small, simple test
+describe('sayHello', function() {
+    it('should return a string "Hello, Alex!"', function () { //or 'should return a string'?
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+});
+//Exercise #5 One more small test before refactoring
+describe('sayHello', function() { //How do I run all tests to newly red/failing?
+    it('should return a string "Hello, Pat!', function () {
+       expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
 });
