c6fc117f30a13f7c8dad672c15a8965ba3189af1
Casanova Geary <CasanovaGeary@Gmail.com>
2022-08-10 16:18:22 -0500

    Is my red cup invisible?

--- code.js
+++ code.js
@@ -11,12 +11,22 @@ const helloWorld = function () {
     return "Hello, World!";
 }
 //Exercise 6, How to improve overall function?
-const sayHello = function(input) {
+/*const sayHello = function(input) {
     if(input === "Alex") {
         return "Hello, Alex!";
     } else if(input === "Pat") {
         return "Hello, Pat!";
-    } else {
+    } else if(input === "Jane!") {
         return "Hello, Jane!";
+    }else {
+        return "Hello, World!";
+    }
+}*/
+//Exercise 6 Refactoring
+function sayHello(input) {
+    if(input === true) {
+        return "Hello, World!";
+} else {
+    return "Hello, " + input + "!";
     }
 }
--- tests.js
+++ tests.js
@@ -24,12 +24,18 @@ describe('sayHello', function() {
     it('should return a string "Hello, Jane!"', function() { //or 'should return a string'?
         expect(sayHello('Jane')).toBe("Hello, Jane!");
     });//Exercise 4
-    it('should return a string "Hello, Jane"', function() {
+    it('should return a string "Hello, Jane!"', function() {
         expect(sayHello('Alex')).toBe("Hello, Alex!");
     });//Exercise 5
     it('should return a string "Hello, Pat!"', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
-    });//Exercise 6
+    });//Exercise 8
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the boolean true', function () {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
 });
 
 
