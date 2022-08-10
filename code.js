9c079040a6527fc6e8f53229d987259ec290911d
Casanova Geary <CasanovaGeary@Gmail.com>
2022-08-10 11:25:38 -0500

    add the first test and solution for intro-to-testing, second try

--- code.js
+++ code.js
@@ -5,7 +5,7 @@
 }
 */
 //Exercise #2, Step 2 I'm not confident doing. How do I make it red, other than making an obvious error?
-function sayHello() {
+/*function sayHello() {
     if(input === "Alex") {
         return "Hello, Alex!";
     } else if(input === "Pat") {
@@ -14,10 +14,17 @@ function sayHello() {
         return "Hello, Jane!";
     }
 }
-//LOST. I just noticed in Jasmine, if I click on "ran 1 0f 9" the result is red! What am I doing wrong?
-//Need instruction for this assignment.
+*/
+
+
 
 //FUNCTION EXPRESSION SYNTAX (ASSIGNING AN ANON FUNCTION TO A VARIABLE)//
+//Exercise #1
 const helloWorld = function () {
     return "Hello, World!";
 }
+//Exercise #2
+const sayHello = function () {
+
+}
+//
